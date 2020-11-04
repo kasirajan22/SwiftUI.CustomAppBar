05a7b12e854df9b6b6f301a01560036997fe0356
kasirajan22 <kasi.srm@gmail.com>
2020-11-04 06:05:16 +0530

    Initial Commit

---
+++ CustomAppBar/CustomAppBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAppBarApp.swift
+//  CustomAppBar
+//
+//  Created by Magizhan on 04/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomAppBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
